bbb116f5b68c62ca5e1970a1ca30f3819b730d3c
shunsuke sasaki <shunsukesasaki@shunsukenoMacBook-Pro.local>
2019-12-19 18:38:53 +0900

    Unification　commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 Rails.application.routes.draw do
-  get 'items/cancel'
-  get 'costomers/edit'
-  get 'costomers/show'
-  get 'costomers/update'
-  get 'costomers/cancel'
+  root 'home#index'
+  get 'a_about' => "home#about"
+
+  resources :costomers, only: [:show, :edit, :update]
+  get "cancel" => 'costomers/cancel'
 
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
     registrations: 'admins/registrations'
   }
-  devise_for :users, controllers: {
+  devise_for :customers, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
 
   resources :items
+  get "cancel" => "items/cancel"
 
   namespace :admin do
     resources :items
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'about' => "home#about"
 end
