5ae04e7f03e941f8a950844beeb9a7cf05b771de
akio5445 <yusa.world2@gmail.com>
2019-12-19 11:02:30 +0000

    add item c index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :cancel]
+
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.new
   end
+
+  def cancel
+    
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+    
+    def item_params
+        params.require(:item).permit(:title, :price, :description, :is_deleted, :image)
+    end
 end
