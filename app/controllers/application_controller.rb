97002087bf9760efc81e1079356d95b990236972
Shunsuke <shun.sskd05@icloud.com>
2019-12-20 04:35:15 +0000

    application_controllerにprotected追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :authenticate_customer!
+
+	def after_sign_in_path_for(resource)
+		root_path
+	end
+
+	def after_log_in_path_for(resource)
+		root_path
+		flash[:notice] = "Signed out successfully"
+	end
+
+	def after_sign_out_path_for(resource)
+		root_path
+	end
+
+	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,
+			:first_name_kana,:phone_number,:customers_postal_code,
+			:customers_address,:email])
+
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:last_name,:first_name,:last_name_kana,
+			:first_name_kana,:phone_number,:customers_postal_code,:customers_address])
+	end
 end
