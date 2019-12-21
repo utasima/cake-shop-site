16f8037e4a7a1b130fe7850069e1391324dc4d4a
akio5445 <yusa.world2@gmail.com>
2019-12-21 08:02:49 +0000

    171221

--- app/controllers/deliver_infos_controller.rb
+++ app/controllers/deliver_infos_controller.rb
@@ -1,9 +1,8 @@
 class DeliverInfosController < ApplicationController
-  before_action :set_deliver_info, only: [:edit, :update, :destroy,]
+  before_action :set_deliver_info, only: [:edit, :destroy, :update]
 
   def index
     @deliver_info = DeliverInfo.all
-    # @deliver_infos = DeliverInfo.find(current_customer.id)
   end
   
   def edit
@@ -14,10 +13,11 @@ class DeliverInfosController < ApplicationController
   end
 
   def create
-    @deliver_infos = DeliverInfo.all
-    @deliver_info = DeliverInfo.new(deliver_info_params)
     # @deliver_info = @customer.deliver_infos.create(published_at: Time.now)
+    @deliver_info = DeliverInfo.new(deliver_info_params)
+    @deliver_info.customer_id = current_customer.id
     if @deliver_info.save
+      @deliver_infos = DeliverInfo.all
       redirect_to deliver_infos_path(@deliver_infos), notice: '住所を追加しました'
     else
       render action: :new
@@ -25,9 +25,9 @@ class DeliverInfosController < ApplicationController
   end
   
   def update
-    @deliver_infos = DeliverInfo.all
     if 
       @deliver_info.update(deliver_info_params)
+      @deliver_infos = DeliverInfo.all
       redirect_to deliver_infos_path(@deliver_infos), notice: '更新しました。'
     else
       render action: :edit
@@ -35,7 +35,6 @@ class DeliverInfosController < ApplicationController
   end
 
   def destroy
-    @deliver_infos = DeliverInfo.all
     @deliver_info.destroy
     redirect_to deliver_infos_path
   end
