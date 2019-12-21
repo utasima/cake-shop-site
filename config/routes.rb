e86c5ab4c98d4c6cebf476ded97ab13ee497c785
Marine Nishikawa <marinenishikawa@marinnoMacBook-Pro.local>
2019-12-21 13:22:25 +0900

    ユーザーshow,edit,パスワード変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'about' => "home#about"
-
+  get 'customer/edit_password' => "customers#edit_password"
+  post'customer/update_password' => "customers#update_password"
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
