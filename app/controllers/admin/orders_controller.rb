308074e736d091003fcb87c5316654f714f72850
Marine Nishikawa <marinenishikawa@marinnoMacBook-Pro.local>
2020-01-05 16:55:10 +0900

    色々修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @orders = Order.with_deleted
+    @orders = Order.all
   end
 
   def show
