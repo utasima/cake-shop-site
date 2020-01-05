599f2f1573db1b83b03625697a48f8abd7578647
Shunsuke <shun.sskd05@icloud.com>
2020-01-05 12:08:58 +0000

    adminログイン後のページリンク先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource_or_scope)
 		if resource_or_scope.is_a?(Admin)
-			admin_items_path
+			 admin_top_path
 		else
 			root_path
 			flash[:notice] = "Signed out successfully"
