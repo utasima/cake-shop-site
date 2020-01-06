f2d507a0fa6ba94ee4d3ab6db74cc9098035326b
時原陸 <roy0817@Tokinomakku.local>
2020-01-06 18:57:12 +0900

    検索機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,9 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource_or_scope)
 		if resource_or_scope.is_a?(Admin)
 			 admin_top_path
-		end
+		else
 			root_path
+		end
 	end
 
 	def after_sign_out_path_for(resource_or_scope)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+This file should contain all the record creation needed to seed the database with its default values.
+The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+Examples:
+
+  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  Character.create(name: 'Luke', movie: movies.first)
 
 
 
-# Customer.create(
-#   email: "test@test.com", password: "password"
-# )
+Customer.create(
+  email: "test@test.com", password: "password"
+)
+
+Item.create!(
+  genre_id: 1,
+  name: 'ショートサンタケーキ',
+  description: 'ちっちゃいサンタさんが乗ったかわいいショートケーキ',
+  price: '7000',
+  image_id: 'images/santa_cake.jpg'
+  
+)
+5.times do |i|
+    Item.create!(genre_id: 1, name: "ケーキ#{i += 1}", description: "美味しいケーキ美味しいケーキ美味しいケーキ", price: 5000)
+  end
 
 
 
-# Item.create!(
-#   genre_id: 1,
-#   name: 'ショートサンタケーキ',
-#   description: 'テスト,テスト,テスト',
-#   price: '70000',
-#   deleted_at: 'true'
-# )
+DeliverInfo.create!(
+  customer_id: 1,
+  name: '田中 勇気',
+  address: '神奈川県横浜市港北区大井戸町26番地123-34',
+  postal_code: '223-3455'
+)
 
-# DeliverInfo.create!(
-#   customer_id: 1,
-#   name: 'ショートショコラ',
-#   address: 'テストテストテスト東京',
-#   postal_code: '2233455'
-
-# )
-
-# Order.create!(
-#   customer_id: 1,
-#   postage: '800',
-#   total_price: '3000',
-#   name: '高木マカダミア',
-#   order_postal_code: '244-4444',
-#   address: '鎌倉県群馬区中央'
-# )
+Order.create!(
+  customer_id: 1,
+  postage: '800',
+  total_price: '3000',
+  name: '高木マカダミア',
+  order_postal_code: '244-4444',
+  address: '鎌倉県群馬区中央'
+)
+OrderItem.create!(
+  customer_id: 1,
+  order_id: 1,
+  item_id: 1,
+  number: 2,
+  price: 14000,
+  making_status: 0
+)
+Genre.create!(
+  [
+    {name: "ケーキ"},
+    {name: "焼き菓子"},
+    {name: "プリン"},
+    {name: "キャンディ"}
+  ]
+)
